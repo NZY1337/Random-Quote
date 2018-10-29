2b30b337666a4ff33eabc4b081a3f1b8d5157b4b
NZY\NZY <mandreicosmin@yahoo.com>
2018-10-29 23:35:20 +0200

    random quote with JSON+AJAX

--- js/random-quote.js
+++ js/random-quote.js
@@ -3,69 +3,34 @@ const BTN = document.getElementById('random-btn');
 let author = document.getElementById('author');
 let mainQuote = document.getElementById('quote');
 
-const QUOTES = [
-    {
-        quote: 'Stay hungry, stay foolish',
-        author: 'Steve Jobs'
-    },
-
-    {
-        quote: "Good Artists Copy, Great Artists Steal.",
-        author : "Pabolo Picasso"
-    },
-
-    {
-        quote: "Argue with idiots, and you become an idiot.",
-        author: "Paul Graham"
-    },
-
-    {
-        quote: "Be yourself, everyone is already taken.",
-        author: "Oscar Wild"
-    },
-
-    {
-        quote: "Simplicty is the ultimate sophistication.",
-        author: "Leonardo Davinci"
-    },
-
-    {
-        quote: "The grass is greener where  your water it.",
-        author: "Neil Barringham"
-    },
-
-    {
-        quote: "Little by little, day by day, what is mean for you will find its WAY",
-        author: "EDLESTER.COM"
-    }
-];
-
-// practic; pushing authors into array;
-let authors = [];
-
-function pushAuthors() {
-    for (let i = 0; i < QUOTES.length; i++) {
-        authors.push(QUOTES[i].author);
-    }
-
-    return authors;
-}
-
-//get random from QUOTES, adding event Listener();
 
 BTN.addEventListener('click', function(){
-    let randomQuote = Math.floor(Math.random() * QUOTES.length);
-    mainQuote.innerText = QUOTES[randomQuote].quote;
-    author.innerHTML = QUOTES[randomQuote].author;
+    
+    // JSON & AJAX - plain JavaScript
+    let myRequest = new XMLHttpRequest();
+
+    // open (takes 2 args - (get-preia / post-trimite), and second argument - link);
+    myRequest.open('GET', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/893105/quotes.json');
+
+    myRequest.onload = function() {
+        let request = JSON.parse(myRequest.responseText);
+        let randomText = Math.floor(Math.random() * request.length);
+        let randomQuote = Math.floor(Math.random() * request.length);
+        
+        outputData(request[randomText], request[randomQuote]);
+    }
+    
+    myRequest.send(); // send the JSON data
+ 
 });
 
 
-console.log(QUOTES);
-
-QUOTES.push({quote: 'hellow', author: 'Schumacher'});
-
-function addQuote(quote, author) {
-    QUOTES.push({quote, author});
+outputData = (text, quote) => {
+      mainQuote.innerText = text.quoteText;
+      author.innerText = quote.quoteAuthor;
 }
 
-addQuote('Maikelele', 'Noir');
+
+
+
+
